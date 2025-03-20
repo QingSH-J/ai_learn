b44d518de6cbf0b50c34b053f1d5d7e9d6b68bf1
QING_SHIYU <qingshiyuu@gmail.com>
2025-03-20 22:54:02 +0800

    third commit

--- main.py
+++ main.py
@@ -39,40 +39,57 @@
 # df["age"].hist()
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.datasets import load_diabetes
+from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import accuracy_score
 
-X, y = load_diabetes(return_X_y=True)
+iris = load_iris()
+X = iris.data
+y = iris.target
 
-X = X[:, [2]]
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=40, shuffle=False)
-model = LinearRegression()
-model.fit(X_train, y_train)
+tree = DecisionTreeRegressor()
 
-y_pred = model.predict(X_test)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-print(f"Mean squared error: {mean_squared_error(y_test, y_pred):.2f}")
-print(f"Coefficient of determination: {r2_score(y_test, y_pred):.2f}")
+tree.fit(X_train, y_train)
 
-fig, ax = plt.subplots(ncols=2, figsize=(10, 5), sharex=True, sharey=True)
-ax[0].scatter(X_train, y_train, label="Train data points")
-ax[0].plot(
-    X_train,
-    model.predict(X_train),
-    linewidth=3,
-    color="tab:orange",
-    label="Model predictions",
-)
-ax[0].set(xlabel="Feature", ylabel="Target", title="Train set")
-ax[0].legend()
+y_pre = tree.predict(X_test)
 
-ax[1].scatter(X_test, y_test, label="Test data points")
-ax[1].plot(X_test, y_pred, linewidth=3, color="tab:orange", label="Model predictions")
-ax[1].set(xlabel="Feature", ylabel="Target", title="Test set")
-ax[1].legend()
+accuracy = accuracy_score(y_test, y_pre)
 
-fig.suptitle("Linear Regression")
+print(accuracy)
 
-plt.show()
+#
+# X, y = load_diabetes(return_X_y=True)
+#
+# X = X[:, [2]]
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=40, shuffle=False)
+# model = LinearRegression()
+# model.fit(X_train, y_train)
+#
+# y_pred = model.predict(X_test)
+#
+# print(f"Mean squared error: {mean_squared_error(y_test, y_pred):.2f}")
+# print(f"Coefficient of determination: {r2_score(y_test, y_pred):.2f}")
+#
+# fig, ax = plt.subplots(ncols=2, figsize=(10, 5), sharex=True, sharey=True)
+# ax[0].scatter(X_train, y_train, label="Train data points")
+# ax[0].plot(
+#     X_train,
+#     model.predict(X_train),
+#     linewidth=3,
+#     color="tab:orange",
+#     label="Model predictions",
+# )
+# ax[0].set(xlabel="Feature", ylabel="Target", title="Train set")
+# ax[0].legend()
+#
+# ax[1].scatter(X_test, y_test, label="Test data points")
+# ax[1].plot(X_test, y_pred, linewidth=3, color="tab:orange", label="Model predictions")
+# ax[1].set(xlabel="Feature", ylabel="Target", title="Test set")
+# ax[1].legend()
+#
+# fig.suptitle("Linear Regression")
+#
+# plt.show()
